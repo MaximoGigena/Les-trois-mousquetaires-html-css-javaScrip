795519b3e2a4fd5859e758fce01bf543957a369b
maxi <gigenamaximo@gmail.com>
2025-01-11 20:57:37 -0300

    Ejercicio-5-css

--- Pantalla Completa/proceso.php
+++ Pantalla Completa/proceso.php
@@ -3,6 +3,6 @@
         $nombre = htmlspecialchars($_POST['nombre'] ?? '');
         $apellido = htmlspecialchars($_POST['apellido']?? '');
     }
-    header("Location: https://www.youtube.com/watch?v=35XFAkwmU4c"); 
+    include 'galeria.html'; 
     exit;
 ?>
